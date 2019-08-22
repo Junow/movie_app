73605101dcf103d0048e9ffbc258049f060acd0b
didi0410 <taristmas@gmail.com>
2019-08-22 18:58:51 +0900

    Updates

---
+++ precache-manifest.46af6e5098b1fd30fd10162442458ebc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa1fdcc30e1e9cbb581c215eb94ed51",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "37b971d4f0c6e80e19a2",
+    "url": "/movie_app/static/css/main.05d37451.chunk.css"
+  },
+  {
+    "revision": "35553d95505140ba87a3",
+    "url": "/movie_app/static/js/2.e5148252.chunk.js"
+  },
+  {
+    "revision": "37b971d4f0c6e80e19a2",
+    "url": "/movie_app/static/js/main.3fdb870b.chunk.js"
+  },
+  {
+    "revision": "5b6a134c98cd9d238fba",
+    "url": "/movie_app/static/js/runtime~main.d0c568e7.js"
+  }
+]);
